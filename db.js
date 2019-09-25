de31d4c17a3498bcd41a598cf3d032b713b86334
cowfarmer <noehyeanhoo@gmail.com>
2019-09-26 01:22:24 +0900

    #2.21 Video Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id:312523,
+    title: 'Video awesome',
+    description: 'This is something I love',
+    views:24,
+    videoFile: "https://archive.org/details/BitBuckBunny_124",
+    creator: {
+      id: 191852,
+      name:"cowfarmer",
+      email:"noehyeanhoo@gmail.com",
+    }
+  },
+  {
+    id:728937,
+    title: 'Video super',
+    description: 'This is something I love',
+    views:24,
+    videoFile: "https://archive.org/details/BitBuckBunny_124",
+    creator: {
+      id: 191852,
+      name:"cowfarmer",
+      email:"noehyeanhoo@gmail.com",
+    }
+  },
+  {
+    id:801981,
+    title: 'Video nice',
+    description: 'This is something I love',
+    views:24,
+    videoFile: "https://archive.org/details/BitBuckBunny_124",
+    creator: {
+      id: 191852,
+      name:"cowfarmer",
+      email:"noehyeanhoo@gmail.com",
+    }
+  },
+  {
+    id:809123,
+    title: 'Video good',
+    description: 'This is something I love',
+    views:24,
+    videoFile: "https://archive.org/details/BitBuckBunny_124",
+    creator: {
+      id: 191852,
+      name:"cowfarmer",
+      email:"noehyeanhoo@gmail.com",
+    }
+  }
+];

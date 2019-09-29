78cec75e4ca8ab56ff27f103ea725be492675e71
cowfarmer <noehyeanhoo@gmail.com>
2019-09-30 01:33:24 +0900

    Connecting to MongoDB and used mongoous

--- db.js
+++ db.js
@@ -1,50 +1,16 @@
-export const videos = [
-  {
-    id: 312523,
-    title: 'Video awesome',
-    description: 'This is something I love',
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 191852,
-      name:"cowfarmer",
-      email:"cow@farmer.com",
-    }
-  },
-  {
-    id: 728937,
-    title: 'Video super',
-    description: 'This is something I love',
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 191852,
-      name:"cowfarmer",
-      email:"cow@farmer.com",
-    }
-  },
-  {
-    id: 801981,
-    title: 'Video nice',
-    description: 'This is something I love',
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 191852,
-      name:"cowfarmer",
-      email:"noehyeanhoo@gmail.com",
-    }
-  },
-  {
-    id: 809123,
-    title: 'Video good',
-    description: 'This is something I love',
-    views: 24,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 191852,
-      name:"cowfarmer",
-      email:"noehyeanhoo@gmail.com",
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}
+);
+
+const db = mongoose.connection;
+
+const hanleOpen = () => console.log("✅  Connected to DB")
+const hanleError = () => console.log(`❌  Error on DB Connection:${error}`)
+
+db.once("open", hanleOpen);
+db.on("error", hanleError);
